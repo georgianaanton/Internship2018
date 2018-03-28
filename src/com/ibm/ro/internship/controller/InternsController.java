6a085b93ffe6de1c05ca5b97eeffbc8892cdf3f1
Georgiana Anton <georgiana.anton@ro.ibm.com>
2018-03-28 17:10:58 +0300

    First commit

---
+++ src/com/ibm/ro/internship/controller/InternsController.java
@@ -0,0 +1,23 @@
+package com.ibm.ro.internship.controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@CrossOrigin
+@RestController
+public class InternsController {
+
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+    public String test() {
+		
+		StringBuilder str = new StringBuilder();
+		str.append("Interns names are:");
+		str.append("Georgiana Anton");
+
+        return str.toString();
+    }
+
+}
